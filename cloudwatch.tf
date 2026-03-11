81f32f0bac5b2e64f9f8ea380446182ca116a5e8
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-03-11 11:06:05 +0530

    Update cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "app_server" {
   ami           = "ami-123456"
   instance_type = "t2.micro"
 }
+
+
+resource "aws_cloudwatch_log_group" "cloudtrail" {
+  name = "cloudtrail-log-group"
+  retention_in_days = 30
+}
