b85999aad229755dbcb966fdf58d56337334bf39
sundeep reddy raju <r.sundeepreddy786@gmail.com>
2026-03-11 11:19:55 +0530

    Update cloudtrail.tf

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,6 +1,9 @@
 resource "aws_cloudtrail" "main" {
-  name                          = "organization-trail"
+  name                          = "main-trail"
   s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.id
   include_global_service_events = true
   is_multi_region_trail         = true
+
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_role.arn
 }
